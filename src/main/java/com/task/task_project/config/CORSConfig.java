d096cb6155df8f2e59d19e93f28ce8657b5c82e5
MarlonPerezR <perezmarlon.dev@gmail.com>
2025-10-27 14:54:10 -0500

    CORSConfig update

--- src/main/java/com/task/task_project/config/CORSConfig.java
+++ src/main/java/com/task/task_project/config/CORSConfig.java
@@ -13,17 +13,17 @@ public class CORSConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOriginPatterns(
                             "http://localhost:3000",
                             "http://localhost:5173",  
                             "http://127.0.0.1:3000",
                             "http://127.0.0.1:5173",
-                            "https://task-project-frontend-fawn.vercel.app/"
+                            "https://task-project-frontend-fawn.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false); // ✅ CAMBIA A false
             }
         };
     }
